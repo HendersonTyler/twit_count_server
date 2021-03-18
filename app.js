a3760ed7416840a7ac0dbd168b33c41207767227
HendersonTyler <henderson.tyler@outlook.com>
2021-03-18 12:24:19 +1000

    Publish

--- app.js
+++ app.js
@@ -2,17 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
+
+app.use(express.static("client/build"));
 
 const wordCloud = require("./components/wordCloud.js");
 const getTweets = require("./components/getTweets.js");
 const sentiment = require("./components/Sentiment.js");
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.get("/user/:user", async (req, res) => {
   try {
@@ -38,5 +36,3 @@ app.get("/user/:user", async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-// process.exit(1);
